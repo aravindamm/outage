aa03bd70c68554eaa0f9bd705daf38ac1ed14a10
aravindamm <50881677+aravindamm@users.noreply.github.com>
2019-05-23 11:04:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pickle
 from flask import Flask, request, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates') 
 
 @app.route('/')
 def home():
