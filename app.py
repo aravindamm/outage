9e14c452ffedc326cb82e8a44cfeb2a419c93781
aravindamm <50881677+aravindamm@users.noreply.github.com>
2019-05-23 14:08:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def OutageFlag():
 
         pkl_file=open('rfmmodel.pkl','rb')
         rfmmodel=pickle.load(pkl_file)
-        test_prediction=rfmmodel.predict(np.array(new_vector))
+        test_prediction=int(rfmmodel.predict(np.array(new_vector).reshape(1,-1)))
         
         return rendor_template('result.html',prediction=test_prediction)
 
